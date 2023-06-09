74e05cf035144e54f8586767f60d68577acfd653
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-06-08 23:59:51 -0400

    Add model for relating menu item offers to the cart

---
+++ src/main/java/com/revature/marstown/entities/CartMenuItemOffer.java
@@ -0,0 +1,34 @@
+package com.revature.marstown.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_menu_item_offers")
+public class CartMenuItemOffer {
+    @Id
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    @JsonBackReference
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_offer_id")
+    @JsonBackReference
+    private MenuItemOffer menuItemOffer;
+}
--- src/main/java/com/revature/marstown/entities/MenuItemOffer.java
+++ src/main/java/com/revature/marstown/entities/MenuItemOffer.java
@@ -1,14 +1,18 @@
 package com.revature.marstown.entities;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -41,4 +45,8 @@ public class MenuItemOffer {
 
     @Column(name = "min_quantity")
     private Integer maximumQuantity;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "menuItemOffer")
+    @JsonManagedReference
+    private Set<CartMenuItemOffer> cartMenuItemOffers;
 }
