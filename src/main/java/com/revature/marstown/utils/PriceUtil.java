1d45f0fd363b234a2248bf2e0ab6dd0dc3ee9670
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-06-21 16:54:24 -0400

    Fix price util, update mayonnaise, update burger combo price from Stripe

--- src/main/java/com/revature/marstown/utils/PriceUtil.java
+++ src/main/java/com/revature/marstown/utils/PriceUtil.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 
 public class PriceUtil {
     public static BigDecimal stripePriceStringToBigDecimal(String stripePriceString) {
+        if (stripePriceString.equals("0")) {
+            stripePriceString = "000";
+        }
         String priceWithDecimal = stripePriceString.substring(0, stripePriceString.length() - 2) + "."
                 + stripePriceString.substring(stripePriceString.length() - 2);
         return new BigDecimal(priceWithDecimal);
